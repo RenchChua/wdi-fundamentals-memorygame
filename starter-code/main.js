19fe2960446cccfb6a5668f040d00f16ccf25277
Rencheng Chua <rench00@gmail.com>
2016-06-28 23:30:17 +0800

    simple control flow added

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,9 @@ var cardOne = "King";
 var cardTwo = "Queen";
 var cardThree = "Queen";
 var cardFour = "King";
+
+if (cardOne===cardFour) {
+  alert("You've found a match.")
+}else {
+  alert("Sorry try again.")
+}
